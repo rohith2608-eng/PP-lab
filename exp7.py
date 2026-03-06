775f6e5898329606b84e012a2dc47a6ee1ee7cc3
ROHITH <rohithperumal63@gmail.com>
2026-03-06 13:29:50 +0530

    Add files via upload

---
+++ exp7.py
@@ -0,0 +1,10 @@
+l=[10,20,30,40,50]
+t=[10,20,30,40,50]
+print("length of the list",len(l))
+print("length of tuple",len(t))
+print("max of the list",max(l))
+print("max of the tuple",max(t))
+print("sum of the list",sum(l))
+print("sum of the tuple",sum(t))
+print("list in sorted order",sorted(l))
+print("tuple in sorted",sorted(t))
